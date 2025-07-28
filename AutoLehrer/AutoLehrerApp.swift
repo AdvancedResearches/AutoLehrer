8d72c35bca84f8f53181ba28ae7ddf568fed5527
Aleksei Khursevich <aleksei.khursevich@web.de>
2025-07-28 16:53:39 +0200

    Initial Commit

---
+++ AutoLehrer/AutoLehrerApp.swift
@@ -0,0 +1,20 @@
+//
+//  AutoLehrerApp.swift
+//  AutoLehrer
+//
+//  Created by Алексей Хурсевич on 28.07.25.
+//
+
+import SwiftUI
+
+@main
+struct AutoLehrerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
