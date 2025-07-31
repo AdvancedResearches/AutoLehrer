6d62bc5f39f9c1a61b89b9b4a890b3671ac02cad
Aleksei Khursevich <aleksei.khursevich@web.de>
2025-07-31 02:16:31 +0200

    1.0.0.0: nomen repeater - 9 - updated nomen picking algorythm

--- AutoLehrer/Extensions/Extensions_Statistics.swift
+++ AutoLehrer/Extensions/Extensions_Statistics.swift
@@ -30,7 +30,7 @@ extension Statistics{
             return
         }
         if let theStatistics = get_statistics(nomen){
-            theStatistics.score = 0
+            theStatistics.score = min(0, theStatistics.score - 1)
             theStatistics.lastAttempt = Date.now
         }else{
             let newStatistics = Statistics(context: context)
@@ -71,7 +71,7 @@ extension Statistics{
         return nil
     }
     public static func pickNomenHive(_ context: NSManagedObjectContext) -> NomenHive{
-        let allTheNomenHives = try! context.fetch(NomenHive.fetchRequest())
+        let allTheNomenHives = try! context.fetch(NomenHive.fetchRequest()).sorted{$0.nomenFrequencyOrder < $1.nomenFrequencyOrder}
         var retValue = allTheNomenHives.first!
         var retScore = nomenHiveUrgency(retValue)
         print("Statistics.pickNomenHive: SET urgency \(retScore) for return \(retValue.nomenFrequencyOrder)")
@@ -87,31 +87,42 @@ extension Statistics{
         return retValue
     }
     public static func nomenHiveUrgency(_ nomenHive: NomenHive) -> Float{
-        print("   Statistics.nomenHiveUrgency: \(nomenHive.nomenFrequencyOrder)")
+        //print("   Statistics.nomenHiveUrgency: \(nomenHive.nomenFrequencyOrder)")
         let allTheFormsOfNomen = nomenHive.relNomen?.allObjects as? [Nomen] ?? []
         let nomenHiveUrgencyMultiplier = Float(1.0 / Float(nomenHive.nomenFrequencyOrder))
-        print("   Statistics.nomenHiveUrgency: frequency order: \(nomenHive.nomenFrequencyOrder)")
+        //print("   Statistics.nomenHiveUrgency: frequency order: \(nomenHive.nomenFrequencyOrder)")
         print("   Statistics.nomenHiveUrgency: urgency multiplier: \(Float(1.0 / Float(nomenHive.nomenFrequencyOrder)))")
         let allTheFormUrgencies: [Float] = allTheFormsOfNomen.map{Statistics.nomenFormScore($0)}
         let maxFormUrgency = Float.maxFromArray(values: allTheFormUrgencies)
-        print("   Statistics.nomenHiveUrgency: max urgency: \(maxFormUrgency)")
+        //print("   Statistics.nomenHiveUrgency: max urgency: \(maxFormUrgency)")
         return nomenHiveUrgencyMultiplier*maxFormUrgency
     }
     public static func nomenFormScore(_ nomen: Nomen) -> Float{
-        print("      Statistics.nomenFormScore: \(nomen.nomen_RU)-\(nomen.nomen_DE)")
+        print("      Statistics.nomenFormScore: \(nomen.nomen_RU!)-\(nomen.nomen_DE!)")
         guard let statisticsForNomen: Statistics = Statistics.get_statistics(nomen) else {
-            print("      Statistics.nomenFormScore: NOT FOUND, SET DEFAULT")
+            //print("      Statistics.nomenFormScore: NOT FOUND, SET DEFAULT")
             Statistics.set_default(nomen)
-            print("      Statistics.nomenFormScore: days offset: \(Date.get_offset_inDays(Date.distantPast, Date.now))")
-            print("      Statistics.nomenFormScore: log(1.0+offset): \(log(1.0 + Float(Date.get_offset_inDays(Date.distantPast, Date.now))))")
-            return log(1.0 + Float(Date.get_offset_inDays(Date.distantPast, Date.now)))
+            let daysSinceLastAttempt = Date.get_offset_inDays(Date.distantPast, Date.now)
+            let decay: Double = exp(0.0)
+            let timeFactor: Double = log(1.0 + Double(daysSinceLastAttempt))
+            print("      Statistics.nomenFormScore: NOT FOUND, SET DEFAULT")
+            print("      Statistics.nomenFormScore: daysSinceLastAttempt = \(daysSinceLastAttempt)")
+            print("      Statistics.nomenFormScore: score = 0")
+            print("      Statistics.nomenFormScore: decay = \(decay)")
+            print("      Statistics.nomenFormScore: timeFactor = \(timeFactor)")
+            print("      Statistics.nomenFormScore: retValue = \(Float(decay*timeFactor))")
+            return Float(decay*timeFactor)
         }
-        print("      Statistics.nomenFormScore: PICKED")
-        print("      Statistics.nomenFormScore: score: \((statisticsForNomen.score))")
-        print("      Statistics.nomenFormScore: effective score: \((1.0/(1.0 + Float(statisticsForNomen.score))))")
-        print("      Statistics.nomenFormScore: days offset: \(Date.get_offset_inDays(Date.distantPast, Date.now))")
-        print("      Statistics.nomenFormScore: log(1.0+offset): \(log(1.0 + Float(Date.get_offset_inDays(Date.distantPast, Date.now))))")
-        print("      Statistics.nomenFormScore: effective score X log: \((1.0/(1.0 + Float(statisticsForNomen.score)))*log(1.0 + Float(Date.get_offset_inDays(statisticsForNomen.lastAttempt ?? Date.distantPast, Date.now))))")
-        return (1.0/(1.0 + Float(statisticsForNomen.score)))*log(1.0 + Float(Date.get_offset_inDays(statisticsForNomen.lastAttempt ?? Date.distantPast, Date.now)))
+        //print("      Statistics.nomenFormScore: PICKED")
+        let daysSinceLastAttempt = Date.get_offset_inDays(statisticsForNomen.lastAttempt ?? Date.distantPast, Date.now)
+        let decay: Double = exp(Double(-statisticsForNomen.score))
+        let timeFactor: Double = log(1.0 + Double(daysSinceLastAttempt))
+        print("      Statistics.nomenFormScore: NOT FOUND, SET DEFAULT")
+        print("      Statistics.nomenFormScore: daysSinceLastAttempt = \(daysSinceLastAttempt)")
+        print("      Statistics.nomenFormScore: score = \(statisticsForNomen.score)")
+        print("      Statistics.nomenFormScore: decay = \(decay)")
+        print("      Statistics.nomenFormScore: timeFactor = \(timeFactor)")
+        print("      Statistics.nomenFormScore: retValue = \(Float(decay*timeFactor))")
+        return Float(decay*timeFactor)
     }
 }
