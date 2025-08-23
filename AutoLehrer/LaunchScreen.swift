3b3e2a9e6da491b59205efb4d11348c472ed6b0f
Aleksei Khursevich <aleksei.khursevich@web.de>
2025-08-23 05:09:24 +0200

    1.1.1.0 - RELEASE

--- AutoLehrer/LaunchScreen.swift
+++ AutoLehrer/LaunchScreen.swift
@@ -5,6 +5,12 @@ struct LaunchScreen: View {
     @EnvironmentObject var theme: ThemeManager
     @EnvironmentObject var presetsProgress: PresetsProgressOO
     
+    var appVersion: String {
+        let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "—"
+        let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "—"
+        return "v\(version).\(build)"
+    }
+    
     var body: some View {
         ZStack {
             theme.currentTheme.NG_LinearGradient_Background_Page.ignoresSafeArea() // Теперь фон на весь экран
@@ -16,9 +22,11 @@ struct LaunchScreen: View {
                     .frame(width: UIScreen.main.bounds.width * 0.9) // 90% ширины экрана
                     .aspectRatio(1, contentMode: .fit) // Высота = ширине (квадрат)
                 
-                Text("Cards RU-DE") // Текст загрузки
+                Text("Карточки - немецкий") // Текст загрузки
                     .NG_textStyling(.NG_TextStyle_Title, theme: theme)
-                    .padding(10)
+                Text(appVersion)
+                        .NG_textStyling(.NG_TextStyle_Text_Small, theme: theme)
+                        .padding(.bottom, 10)
                 Text("Запускается...")
                     .NG_textStyling(.NG_TextStyle_Text_Small, theme: theme)
                     .padding(10)
