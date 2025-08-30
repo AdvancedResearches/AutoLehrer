376a120e2283ad457acd42cad14384f105d4fbb7
Aleksei Khursevich <aleksei.khursevich@web.de>
2025-08-30 15:10:09 +0200

    1.2.0.0 - KICK OFF

--- AutoLehrer/Extensions/Extensions_Beispiel.swift
+++ AutoLehrer/Extensions/Extensions_Beispiel.swift
@@ -8,6 +8,7 @@
 import CoreData
 
 extension Beispiel{
+    /*
     public static func findOrCreate(in context: NSManagedObjectContext, withName_DE name_DE: String) -> Beispiel {
         var result: Beispiel? = nil
         do{
@@ -17,6 +18,20 @@ extension Beispiel{
             }
         }catch{}
         return result!
+    }
+     */
+    public static func findOrCreate(in context: NSManagedObjectContext, withName_DE name_DE: String) -> Beispiel {
+        let request: NSFetchRequest<Beispiel> = Beispiel.fetchRequest()
+        request.predicate = NSPredicate(format: "beispiel_DE == %@", name_DE)
+        request.fetchLimit = 1
+
+        if let result = try? context.fetch(request).first {
+            return result
+        } else {
+            let newObject = Beispiel(context: context)
+            newObject.beispiel_DE = name_DE
+            return newObject
+        }
     }
 }
 
