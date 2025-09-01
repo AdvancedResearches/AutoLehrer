24c405ccf9f29265c4688bf8a2b0e6f7cafcb972
Aleksei Khursevich <aleksei.khursevich@web.de>
2025-09-01 19:46:29 +0200

    1.3.0.0 - repeater - major refactoring - 6 - extracteed buttons

--- AutoLehrer/Repeater/WortRepeater.swift
+++ AutoLehrer/Repeater/WortRepeater.swift
@@ -364,6 +364,219 @@ struct WortRepeater: View {
             }
         }
     }
+    private func NextButton_Regular() -> some View{
+        Group{
+            let successFormen = guessingResult.filter{$0 == 1}.count
+            let checkedFormen = guessingResult.count
+            NG_Button(
+                title: "Дальше (\(successFormen)/\(checkedFormen) было правильно)".localized(for: language),
+                style: successFormen==checkedFormen ? .NG_ButtonStyle_Green : .NG_ButtonStyle_Red,
+                isDisabled: .init(
+                    get: { !readyToMoveOn },
+                    set: { readyToMoveOn = !$0 }
+                ),
+                isHighlighting: .constant(false),
+                isPulsating: .constant(readyToMoveOn),
+                action: {
+                    if(readyToMoveOn){
+                        if(prufungModus){
+                            prufungResult.updateValue(guessingResult.filter{$0 == 1}.count, forKey: runningWortArt!)
+                        }else{
+                            attemptCounter += 1
+                            var successCounter: Int = 0
+                            
+                            for theFormCounter in 0..<wort.count{
+                                if(guessingResult[theFormCounter] == 1){
+                                    Statistics.set_success(wort[theFormCounter])
+                                    successCounter += 1
+                                }
+                                if(guessingResult[theFormCounter] == -1){
+                                    Statistics.set_failure(wort[theFormCounter])
+                                }
+                            }
+                            
+                            if(successCounter == wort.count){
+                                if(WortFormen.set_success(pickedWortFormen!)){
+                                    confirmedWorte.insert(pickedWortFormen!)
+                                }
+                            }else{
+                                WortFormen.set_failure(pickedWortFormen!)
+                                confirmedWorte.remove(pickedWortFormen!)
+                            }
+                            
+                            WortFormen.set_attempted(pickedWortFormen!)
+                            Statistics.wortFormenUrgency(pickedWortFormen!)
+                        }
+                        
+                        if(prufungModus){
+                            pickTheWordFurPrufung()
+                        }else{
+                            pickTheWord()
+                        }
+                    }else{
+                        withAnimation(.easeOut(duration: 0.1)) { scaleRatio = 1.1 }
+                        withAnimation(.easeOut(duration: 0.1).delay(0.1)) { scaleRatio = 1 }
+                        withAnimation(.easeOut(duration: 0.1).delay(0.2)) { scaleRatio = 1.1 }
+                        withAnimation(.easeOut(duration: 0.1).delay(0.3)) { scaleRatio = 1 }
+                        withAnimation(.easeOut(duration: 0.1).delay(0.4)) { scaleRatio = 1.1 }
+                        withAnimation(.easeOut(duration: 0.5).delay(0.5)) { scaleRatio = 1 }
+                    }
+                },
+                widthFlood: true
+            )
+            .padding(.horizontal, 15)
+            .padding(.vertical, 25)
+            .transition(.scale)
+        }
+    }
+    private func NextButton_Prufung_Skip() -> some View{
+        Group{
+            let successFormen = guessingResult.filter{$0 == 1}.count
+            let checkedFormen = guessingResult.count
+            NG_Button(
+                title: "К следующему разделу".localized(for: language),
+                style: successFormen==checkedFormen ? .NG_ButtonStyle_Green : .NG_ButtonStyle_Red,
+                isDisabled: .init(
+                    get: { !readyToMoveOn },
+                    set: { readyToMoveOn = !$0 }
+                ),
+                isHighlighting: .constant(false),
+                isPulsating: .constant(readyToMoveOn),
+                action: {
+                    if(readyToMoveOn){
+                        if(prufungModus){
+                            prufungResult.updateValue(guessingResult.filter{$0 == 1}.count, forKey: runningWortArt!)
+                        }else{
+                            attemptCounter += 1
+                            var successCounter: Int = 0
+                            
+                            for theFormCounter in 0..<wort.count{
+                                if(guessingResult[theFormCounter] == 1){
+                                    Statistics.set_success(wort[theFormCounter])
+                                    successCounter += 1
+                                }
+                                if(guessingResult[theFormCounter] == -1){
+                                    Statistics.set_failure(wort[theFormCounter])
+                                }
+                            }
+                            
+                            if(successCounter == wort.count){
+                                if(WortFormen.set_success(pickedWortFormen!)){
+                                    confirmedWorte.insert(pickedWortFormen!)
+                                }
+                            }else{
+                                WortFormen.set_failure(pickedWortFormen!)
+                                confirmedWorte.remove(pickedWortFormen!)
+                            }
+                            
+                            WortFormen.set_attempted(pickedWortFormen!)
+                            Statistics.wortFormenUrgency(pickedWortFormen!)
+                        }
+                        
+                        if(prufungModus){
+                            pickTheWordFurPrufung()
+                        }else{
+                            pickTheWord()
+                        }
+                    }else{
+                        withAnimation(.easeOut(duration: 0.1)) { scaleRatio = 1.1 }
+                        withAnimation(.easeOut(duration: 0.1).delay(0.1)) { scaleRatio = 1 }
+                        withAnimation(.easeOut(duration: 0.1).delay(0.2)) { scaleRatio = 1.1 }
+                        withAnimation(.easeOut(duration: 0.1).delay(0.3)) { scaleRatio = 1 }
+                        withAnimation(.easeOut(duration: 0.1).delay(0.4)) { scaleRatio = 1.1 }
+                        withAnimation(.easeOut(duration: 0.5).delay(0.5)) { scaleRatio = 1 }
+                    }
+                },
+                widthFlood: true
+            )
+            .padding(.horizontal, 15)
+            .padding(.vertical, 25)
+            .transition(.scale)
+        }
+    }
+    private func NextButton_Prufung_Next() -> some View{
+        Group{
+            let successFormen = guessingResult.filter{$0 == 1}.count
+            let checkedFormen = guessingResult.count
+            NG_Button(
+                title: "Дальше (\(successFormen)/\(checkedFormen) было правильно)".localized(for: language),
+                style: successFormen==checkedFormen ? .NG_ButtonStyle_Green : .NG_ButtonStyle_Red,
+                isDisabled: .init(
+                    get: { !readyToMoveOn },
+                    set: { readyToMoveOn = !$0 }
+                ),
+                isHighlighting: .constant(false),
+                isPulsating: .constant(readyToMoveOn),
+                action: {
+                    if(readyToMoveOn){
+                        if(prufungModus){
+                            prufungResult.updateValue(guessingResult.filter{$0 == 1}.count, forKey: runningWortArt!)
+                        }else{
+                            attemptCounter += 1
+                            var successCounter: Int = 0
+                            
+                            for theFormCounter in 0..<wort.count{
+                                if(guessingResult[theFormCounter] == 1){
+                                    Statistics.set_success(wort[theFormCounter])
+                                    successCounter += 1
+                                }
+                                if(guessingResult[theFormCounter] == -1){
+                                    Statistics.set_failure(wort[theFormCounter])
+                                }
+                            }
+                            
+                            if(successCounter == wort.count){
+                                if(WortFormen.set_success(pickedWortFormen!)){
+                                    confirmedWorte.insert(pickedWortFormen!)
+                                }
+                            }else{
+                                WortFormen.set_failure(pickedWortFormen!)
+                                confirmedWorte.remove(pickedWortFormen!)
+                            }
+                            
+                            WortFormen.set_attempted(pickedWortFormen!)
+                            Statistics.wortFormenUrgency(pickedWortFormen!)
+                        }
+                        
+                        if(prufungModus){
+                            pickTheWordFurPrufung()
+                        }else{
+                            pickTheWord()
+                        }
+                    }else{
+                        withAnimation(.easeOut(duration: 0.1)) { scaleRatio = 1.1 }
+                        withAnimation(.easeOut(duration: 0.1).delay(0.1)) { scaleRatio = 1 }
+                        withAnimation(.easeOut(duration: 0.1).delay(0.2)) { scaleRatio = 1.1 }
+                        withAnimation(.easeOut(duration: 0.1).delay(0.3)) { scaleRatio = 1 }
+                        withAnimation(.easeOut(duration: 0.1).delay(0.4)) { scaleRatio = 1.1 }
+                        withAnimation(.easeOut(duration: 0.5).delay(0.5)) { scaleRatio = 1 }
+                    }
+                },
+                widthFlood: true
+            )
+            .padding(.horizontal, 15)
+            .padding(.vertical, 25)
+            .transition(.scale)
+        }
+    }
+    private func NextButton_Prufung_Ende() -> some View{
+        Group{
+            NG_Button(
+                title: "Экзамен закончен".localized(for: language),
+                style: .NG_ButtonStyle_Regular,
+                isDisabled: .constant(false),
+                isHighlighting: .constant(false),
+                isPulsating: .constant(true),
+                action: {
+                    dismiss()
+                },
+                widthFlood: true
+            )
+            .padding(.horizontal, 15)
+            .padding(.vertical, 25)
+            .transition(.scale)
+        }
+    }
     private func PrufungCompletedSection() -> some View{
         Group{
             ScrollViewReader { proxy in
@@ -417,9 +630,6 @@ struct WortRepeater: View {
                         .padding(.horizontal, 15)
                         .padding(.vertical, 25)
                         .transition(.scale)
-                        .onAppear{
-                            print("prufungConpleted button shall be rendered")
-                        }
                     }
                 }
             }
